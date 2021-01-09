be0db12df8391879f8a6104b22cb36910d2577a4
Kornflex28 <chauvet.louis@gmail.com>
2021-01-09 21:47:26 +0100

    hp2 and hp3

--- main.py
+++ main.py
@@ -7,9 +7,31 @@ import csv
 if __name__ == "__main__":
 
     str_filter = "[\(\[\{].*?[\)\]\}]"
-    character_names_map = {'Dumbledore': "Albus Dumbledore", "Flint": "Marcus Flint", 'McGonagall': "Minerva McGonagall", 'Hagrid': "Rubeus Hagrid", 'Petunia': "Petunia Dursley", 'Dudley': "Dudley Dursley", 'Vernon': "Vernon Dursley", 'Harry': "Harry Potter", 'Quirrell': "Quirinus Quirrell", 'Ollivander': "Garrick Ollivander", 'Mrs. Weasley': "Molly Weasley", 'George': "George Weasley", 'Fred': "Fred Weasley",
+    character_names_map = {"Pettigrew":"Peter Pettigrew","Sirius":"Sirius Black", 'Parvati':"Parvati Patil", 'Pansy':"Pansy Parkinson","Dean":"Dean Thomas", "Professor trelawney":"Sybill Trelawney", "Malfoy":"Draco Malfoy", "Lupin":"Remus Lupin", "Molly":"Molly Weasley", "Arthur":"Arthur Weasley","Fudge":"Cornelius Fudge","Marge":"Marge Dursley","Goyle":"Gregory Goyle","Colin":"Collin Creevey","Crabbe":"Vincent Crabbe","Professor McGonagall":"Minerva McGonagall","Lucius":"Lucius Malfoy", "Riddle":"Tom Riddle","Myrtle":"Moaning Myrtle","Madam Pomfrey":"Poppy Pomfrey","Professor Sprout":"Pomona Sprout","Lockhart":"Gilderoy Lockhart","Mr. Weasley":"Arthur Weasley",'Dumbledore': "Albus Dumbledore", "Flint": "Marcus Flint", 'McGonagall': "Minerva McGonagall", 'Hagrid': "Rubeus Hagrid", 'Petunia': "Petunia Dursley", 'Dudley': "Dudley Dursley", 'Vernon': "Vernon Dursley", 'Harry': "Harry Potter", 'Quirrell': "Quirinus Quirrell", 'Ollivander': "Garrick Ollivander", 'Mrs. Weasley': "Molly Weasley", 'George': "George Weasley", 'Fred': "Fred Weasley",
                            'Ginny': "Ginny Weasley", 'Ron': "Ron Weasley", 'Hermione': "Hermione Granger", 'Neville': "Neville Longbottom", 'Draco': "Draco Malfoy", 'Seamus': "Seamus Finnigan", 'Percy': "Percy Weasley", 'Nick': "Nearly Headless Nick", 'Snape': "Severus Snape", 'Hooch': "Rolanda Hooch", 'Filch': "Argus Filch", 'Oliver': "Oliver Wood", 'Flitwick': "Filius Flitwick", 'Lee': "Lee Jordan", 'Unseen Inhuman Voice': "Voldemort"}
-    filename = "hp1"
+    filename = "hp3"
+
+    # with open(f"scripts/{filename}.txt", encoding="utf8") as f:
+    #     script_lines = list(map(str.strip, re.sub(
+    #         str_filter, "", f.read()).splitlines()))
+    # # print(script_lines)
+    # ch = "Harry"
+    # test = []
+    # l=""
+    # for line in script_lines:
+    #     if ":" in line:
+    #         test.append(l)
+    #         ch = line.split(":",1)[0].strip().capitalize()
+    #         rest = line.split(":",1)[1].strip()
+    #         l = f"{ch}: {rest}"
+    #     else:
+    #         l += f" {line.strip()}"
+        
+
+    # with open(f"scripts/{filename}_test.txt","w", encoding="utf8") as f:
+    #     for l in test:
+    #         f.write(l.replace('"',"")+'\n')
+
 
     with open(f"scripts/{filename}.txt", encoding="utf8") as f:
         script_lines = list(map(str.strip, re.sub(
@@ -37,10 +59,11 @@ if __name__ == "__main__":
                     movie_data.append(
                         {'movie': movie, 'chapter': chapter, 'character': character, 'dialog': dialog})
     movie_df = pd.DataFrame(movie_data)
-    movie_df.to_csv(f"formatted/{filename}.csv",
+    movie_df.to_csv(f"datasets/{filename}.csv",
                     quoting=csv.QUOTE_ALL, index=False)
 
     # Check if missing chapter in script original text file
     # print(chapters-set(movie_df['chapter'].unique()))
 
     print(movie_df.character.unique())
+    print(movie_df.head())
