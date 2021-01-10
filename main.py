4a5b1d1f231c879dcf1edafee39fb5eca521796c
Kornflex28 <chauvet.louis@gmail.com>
2021-01-10 20:52:41 +0100

    hp7

--- main.py
+++ main.py
@@ -7,14 +7,13 @@ import csv
 if __name__ == "__main__":
 
     str_filter = "[\(\[\{].*?[\)\]\}]"
-    character_names_map = {"Barty jr":"Bartemius Crouch Junior","Fleur":"Fleur Delacour","Nigel":"Nigel Wolpert","Viktor":"Viktor Krum","Rita":"Rita Skeeter","Mad-eye":"Alastor Moody","Severus":"Severus Snape","Igor":"Igor Karkaroff","Cho":"Cho Chang","Barty":"Bartemius Crouch","Cedric":"Cedric Diggory","Amos":"Amos Diggory", "Pettigrew":"Peter Pettigrew","Sirius":"Sirius Black",'Padma':"Padma Patil", 'Parvati':"Parvati Patil", 'Pansy':"Pansy Parkinson","Dean":"Dean Thomas", "Professor trelawney":"Sybill Trelawney", "Malfoy":"Draco Malfoy", "Lupin":"Remus Lupin", "Molly":"Molly Weasley", "Arthur":"Arthur Weasley","Fudge":"Cornelius Fudge","Marge":"Marge Dursley","Goyle":"Gregory Goyle","Colin":"Collin Creevey","Crabbe":"Vincent Crabbe","Professor McGonagall":"Minerva McGonagall","Lucius":"Lucius Malfoy", "Riddle":"Tom Riddle","Myrtle":"Moaning Myrtle","Madam Pomfrey":"Poppy Pomfrey","Professor Sprout":"Pomona Sprout","Lockhart":"Gilderoy Lockhart","Mr. Weasley":"Arthur Weasley",'Dumbledore': "Albus Dumbledore", "Flint": "Marcus Flint", 'McGonagall': "Minerva McGonagall", 'Hagrid': "Rubeus Hagrid", 'Petunia': "Petunia Dursley", 'Dudley': "Dudley Dursley", 'Vernon': "Vernon Dursley", 'Harry': "Harry Potter", 'Quirrell': "Quirinus Quirrell", 'Ollivander': "Garrick Ollivander", 'Mrs. Weasley': "Molly Weasley", 'George': "George Weasley", 'Fred': "Fred Weasley",
+    character_names_map = {"Kingsley":"Kingsley Shacklebolt","Narcissa":"Narcissa Malfoy","Dolores umbridge":"Dolores Umbridge","Umbridge":"Dolores Umbridge","Lily":"Lily Potter","Elphias doge":"Elphias Doge","Xenophilius lovegood":"Xenophilius Lovegood","Luna":"Luna Lovegood","Mrs. weasley":"Molly Weasley","Death eater":"Death Eater","Mundungus":"Mundungus Fletcher","Tonks":"Nymphadora Tonks","Bill":"Bill Weasley","Aunt petunia":"Petunia Dursley","Uncle vernon":"Vernon Dursley","Wormtail":"Peter Pettigrew","Bellatrix":"Bellatrix Lestrange","Thicknesse":"Pius Thicknesse","Yaxley":"Corban Yaxley","Mr. granger":"Mr. Granger","Mrs. granger":"Mrs. Granger","Scrimgeour":"Rufus Scrimgeour","Barty jr":"Bartemius Crouch Junior","Fleur":"Fleur Delacour","Nigel":"Nigel Wolpert","Viktor":"Viktor Krum","Rita":"Rita Skeeter","Mad-eye":"Alastor Moody","Severus":"Severus Snape","Igor":"Igor Karkaroff","Cho":"Cho Chang","Barty":"Bartemius Crouch","Cedric":"Cedric Diggory","Amos":"Amos Diggory", "Pettigrew":"Peter Pettigrew","Sirius":"Sirius Black",'Padma':"Padma Patil", 'Parvati':"Parvati Patil", 'Pansy':"Pansy Parkinson","Dean":"Dean Thomas", "Professor trelawney":"Sybill Trelawney", "Malfoy":"Draco Malfoy", "Lupin":"Remus Lupin", "Molly":"Molly Weasley", "Arthur":"Arthur Weasley","Fudge":"Cornelius Fudge","Marge":"Marge Dursley","Goyle":"Gregory Goyle","Colin":"Collin Creevey","Crabbe":"Vincent Crabbe","Professor McGonagall":"Minerva McGonagall","Lucius":"Lucius Malfoy", "Riddle":"Tom Riddle","Myrtle":"Moaning Myrtle","Madam Pomfrey":"Poppy Pomfrey","Professor Sprout":"Pomona Sprout","Lockhart":"Gilderoy Lockhart","Mr. Weasley":"Arthur Weasley",'Dumbledore': "Albus Dumbledore", "Flint": "Marcus Flint", 'McGonagall': "Minerva McGonagall", 'Hagrid': "Rubeus Hagrid", 'Petunia': "Petunia Dursley", 'Dudley': "Dudley Dursley", 'Vernon': "Vernon Dursley", 'Harry': "Harry Potter", 'Quirrell': "Quirinus Quirrell", 'Ollivander': "Garrick Ollivander", 'Mrs. Weasley': "Molly Weasley", 'George': "George Weasley", 'Fred': "Fred Weasley",
                            'Ginny': "Ginny Weasley", 'Ron': "Ron Weasley", 'Hermione': "Hermione Granger", 'Neville': "Neville Longbottom", 'Draco': "Draco Malfoy", 'Seamus': "Seamus Finnigan", 'Percy': "Percy Weasley", 'Nick': "Nearly Headless Nick", 'Snape': "Severus Snape", 'Hooch': "Rolanda Hooch", 'Filch': "Argus Filch", 'Oliver': "Oliver Wood", 'Flitwick': "Filius Flitwick", 'Lee': "Lee Jordan", 'Unseen Inhuman Voice': "Voldemort"}
-    filename = "hp6"
+    filename = "hp7"
 
     # with open(f"scripts/{filename}_test.txt", encoding="utf8") as f:
     #     script_lines = list(map(str.strip, re.sub(
     #         str_filter, "", f.read()).splitlines()))
-    # print(script_lines)
 
     # dialogs = []
     # is_speaking,dialog,ch = False,"",""
@@ -29,6 +28,7 @@ if __name__ == "__main__":
     #         is_speaking = False
     #     elif is_speaking :
     #         dialog+=" "+line
+    # dialogs.append({'ch':ch,'dialog':dialog})
     # print(set(list(map(lambda x: x['ch'], dialogs))))
         
 
