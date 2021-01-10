7e35379a7ca7ae2d842c6736f719afa596b5b9d7
Kornflex28 <chauvet.louis@gmail.com>
2021-01-10 19:51:15 +0100

    hp6

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
     str_filter = "[\(\[\{].*?[\)\]\}]"
     character_names_map = {"Barty jr":"Bartemius Crouch Junior","Fleur":"Fleur Delacour","Nigel":"Nigel Wolpert","Viktor":"Viktor Krum","Rita":"Rita Skeeter","Mad-eye":"Alastor Moody","Severus":"Severus Snape","Igor":"Igor Karkaroff","Cho":"Cho Chang","Barty":"Bartemius Crouch","Cedric":"Cedric Diggory","Amos":"Amos Diggory", "Pettigrew":"Peter Pettigrew","Sirius":"Sirius Black",'Padma':"Padma Patil", 'Parvati':"Parvati Patil", 'Pansy':"Pansy Parkinson","Dean":"Dean Thomas", "Professor trelawney":"Sybill Trelawney", "Malfoy":"Draco Malfoy", "Lupin":"Remus Lupin", "Molly":"Molly Weasley", "Arthur":"Arthur Weasley","Fudge":"Cornelius Fudge","Marge":"Marge Dursley","Goyle":"Gregory Goyle","Colin":"Collin Creevey","Crabbe":"Vincent Crabbe","Professor McGonagall":"Minerva McGonagall","Lucius":"Lucius Malfoy", "Riddle":"Tom Riddle","Myrtle":"Moaning Myrtle","Madam Pomfrey":"Poppy Pomfrey","Professor Sprout":"Pomona Sprout","Lockhart":"Gilderoy Lockhart","Mr. Weasley":"Arthur Weasley",'Dumbledore': "Albus Dumbledore", "Flint": "Marcus Flint", 'McGonagall': "Minerva McGonagall", 'Hagrid': "Rubeus Hagrid", 'Petunia': "Petunia Dursley", 'Dudley': "Dudley Dursley", 'Vernon': "Vernon Dursley", 'Harry': "Harry Potter", 'Quirrell': "Quirinus Quirrell", 'Ollivander': "Garrick Ollivander", 'Mrs. Weasley': "Molly Weasley", 'George': "George Weasley", 'Fred': "Fred Weasley",
                            'Ginny': "Ginny Weasley", 'Ron': "Ron Weasley", 'Hermione': "Hermione Granger", 'Neville': "Neville Longbottom", 'Draco': "Draco Malfoy", 'Seamus': "Seamus Finnigan", 'Percy': "Percy Weasley", 'Nick': "Nearly Headless Nick", 'Snape': "Severus Snape", 'Hooch': "Rolanda Hooch", 'Filch': "Argus Filch", 'Oliver': "Oliver Wood", 'Flitwick': "Filius Flitwick", 'Lee': "Lee Jordan", 'Unseen Inhuman Voice': "Voldemort"}
-    filename = "hp5"
+    filename = "hp6"
 
-    with open(f"scripts/{filename}_test.txt", encoding="utf8") as f:
-        script_lines = list(map(str.strip, re.sub(
-            str_filter, "", f.read()).splitlines()))
+    # with open(f"scripts/{filename}_test.txt", encoding="utf8") as f:
+    #     script_lines = list(map(str.strip, re.sub(
+    #         str_filter, "", f.read()).splitlines()))
     # print(script_lines)
 
     # dialogs = []
@@ -29,11 +29,11 @@ if __name__ == "__main__":
     #         is_speaking = False
     #     elif is_speaking :
     #         dialog+=" "+line
-    # # print(set(list(map(lambda x: x['ch'], dialogs))))
+    # print(set(list(map(lambda x: x['ch'], dialogs))))
         
 
     # with open(f"scripts/{filename}.txt","w", encoding="utf8") as f:
-    #     f.write("Harry Potter and the Order of the Phoenix\n")
+    #     f.write("Harry Potter and the Half-Blood Prince\n")
     #     for l in dialogs:
     #         f.write(f"{l['ch']}:{l['dialog']}\n")
 
